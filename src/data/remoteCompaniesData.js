c59c3bd123fb0172e5b4d3d434de0142f0efb8fc
Yashvi-Khandelwal <khandelwalyashvi427@gmail.com>
2025-10-01 15:19:25 +0530

    remote section started

---
+++ src/data/remoteCompaniesData.js
@@ -0,0 +1,14 @@
+// src/data/RemoteCompanies.js
+
+export const remoteCompanies = [
+ { id: 1, name: "Superside" },
+  { id: 2, name: "Andela" },
+  { id: 3, name: "Howdy.com" },
+  { id: 4, name: "Remote" },
+  { id: 5, name: "Torre.ai" },
+  { id: 6, name: "Hubstaff" },
+  { id: 7, name: "Zapier" },
+  { id: 8, name: "GitLab" },
+  { id: 9, name: "Automattic" },
+  { id: 10, name: "InVision" }
+];
--- src/pages/Remote.jsx
+++ src/pages/Remote.jsx
@@ -1,7 +1,23 @@
+import "./Remote.css";
+import { remoteCompanies } from "../data/remoteCompaniesData";
+
 export const Remote = () =>{
     return(
-        <>
-            <h1>Remote page</h1>
-        </>
+           <section className="remote-container">
+      <div className="remote-hero">
+        <h1>Explore Remote Opportunities</h1>
+        <p>
+          Discover companies offering remote positions across domains. This section lists publicly available company names that provide remote work options. The list is small now but scalable for future updates.
+        </p>
+      </div>
+
+      <div className="remote-grid">
+        {remoteCompanies.map((company) => (
+          <div key={company.id} className="remote-card">
+            {company.name}
+          </div>
+        ))}
+      </div>
+    </section>
     )
 }
